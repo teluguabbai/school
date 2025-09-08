5cf7a5f67cf6cb9c36d4e2f020fc478933b97675
parasuram <parasu7824@gmail.com>
2025-09-08 15:56:30 +0530

    Update settings.py

--- myschool/settings.py
+++ myschool/settings.py
@@ -84,11 +84,14 @@ WSGI_APPLICATION = 'myschool.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=not DEBUG,
+    )
 }
 
 
