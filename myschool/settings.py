8a690ef928b1ed79d6c8d22288dc2416ba4adae7
parasuram <parasu7824@gmail.com>
2025-09-09 09:06:56 +0530

    Update settings.py

--- myschool/settings.py
+++ myschool/settings.py
@@ -90,11 +90,10 @@ WSGI_APPLICATION = 'myschool.wsgi.application'
 
 
 
+
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=not DEBUG,
+        default=os.environ.get("DATABASE_URL")
     )
 }
 
